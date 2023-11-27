ebfa2fdbcc6788f24864b38df33d5cd7d47bcfe4
jonathan <jonathansusanto2015@gmail.com>
2023-11-27 13:02:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
+
+const baseURL = "http://testapi.test/api/books";
 
 function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(`${baseURL}/20`).then((response) => {
+      setPost(response.data)
+    });
+  }, []);
+
+  if (!post) return null;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{post.name}</h1>
     </div>
   );
 }
